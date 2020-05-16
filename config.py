0968aad919d17dc3d8c1755b6f7823931177f761
Chiel Heldoorn <chiel@mysocialdatabase.com>
2020-05-16 21:14:31 +0200

    Added ability to supress progressbar (for usage in cluster)

--- config.py
+++ config.py
@@ -55,9 +55,9 @@ train_arg.add_argument('--gamma', type=float, default=0.1,
 train_arg.add_argument('--lr_step', type=int, default=60,
                        help='number of epochs after which the lr is multiplied with gamma')
 train_arg.add_argument('--lambda_a', type=float, default=0.5,
-                       help='balance between sl signal and the additional signals')
+                       help='balance between sl signal and the additional signals, 1.0 is solely SL signal, 0.0 is no signal (not recommanded)')
 train_arg.add_argument('--lambda_b', type=float, default=0.5,
-                       help='balance between kd signal and the dml signals')
+                       help='balance between DML signal and the KD signals, 1.0 is no KD signal, 0.0 is no DML signal')
 train_arg.add_argument('--temperature', type=float, default=3,
                        help='softmax temperature')
 
@@ -67,7 +67,7 @@ misc_arg.add_argument('--disable_cuda', type=str2bool, default=False,
                       help="Whether disable the GPU, if False, GPU will be utilized if available")
 misc_arg.add_argument('--best', type=str2bool, default=False,
                       help='Load best model or most recent for testing')
-misc_arg.add_argument('--random_seed', type=int, default=1,
+misc_arg.add_argument('--random_seed', type=int, default=2020,
                       help='Seed to ensure reproducibility')
 misc_arg.add_argument('--data_dir', type=str, default='./data/cifar100',
                       help='Directory in which data is stored')
@@ -75,6 +75,8 @@ misc_arg.add_argument('--use_wandb', type=str2bool, default=False,
                       help='Whether to use Weights and Biases for visualization')
 misc_arg.add_argument('--resume', type=str2bool, default=False,
                       help='Whether to resume training from checkpoint')
+misc_arg.add_argument('--progress_bar', type=str2bool, default=False,
+                      help='Whether to log the progress of the training phase')
 misc_arg.add_argument('--experiment_name', type=str, default='test_experiment',
                       help='Name of the experiment, used to store all logs and checkpoints')
 misc_arg.add_argument('--experiment_level', type=int, default=1,
@@ -82,7 +84,7 @@ misc_arg.add_argument('--experiment_level', type=int, default=1,
 misc_arg.add_argument('--model_names', nargs='+', default=['RN14', 'MN20', 'EFB0'],
                       help='The abbreviation of the model name with size indicator',
                       choices=['EFB0', 'EFB1', 'EFB2', 'EFB3', 'EFB4', 'EFB5', 'EFB6', 'EFB7',
-                               'MN20', 'MN30', 'MN40', 'MN50', 'MN60', 'MN70', 'MN85', 'MN100',
+                               'MN20', 'MN30', 'MN40', 'MN50', 'MN60', 'MN70', 'MN85', 'MN100', 'MN125', 'MN150', 'MN200',
                                'RN14', 'RN20', 'RN32', 'RN44', 'RN50', 'RN110', 'RN200', 'RN302',
                                'CN2', 'CN4', 'CN6', 'CN28', 'CN10'])
 
