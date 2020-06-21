ca1a915b60ea4d3bee058d01cd27789cebf256d9
Chiel Heldoorn <chiel@mysocialdatabase.com>
2020-06-21 22:40:31 +0200

    BUG FIX: made CUDA kernels deterministic to ensure reproducibility, ADDED: option to disable the progress bar and the option to load the previous level of another experiment into the current experiment to load the teachers from.

--- main.py
+++ main.py
@@ -9,59 +9,70 @@ from data_loader import get_test_loader, get_train_loader
 
 
 def main(config):
-    if config.copy_first_level_from != 'self':
+    if config.previous_level_from != 'self':
         copy_first_level(
-            src_exp=config.copy_first_level_from,
+            src_exp=config.previous_level_from,
             dst_exp=config.experiment_name
         )
         sys.exit(
-            f'First level copied from {config.copy_first_level_from}, done for the first level...')
+            f'First level copied from {config.previous_level_from}, done for the first level...')
 
     # ensure reproducibility
     torch.manual_seed(config.random_seed)
     numpy.random.seed(config.random_seed)
     random.seed(config.random_seed)
 
-    kwargs = {}
     if not config.disable_cuda and torch.cuda.is_available():
+        torch.backends.cudnn.deterministic = True
+        torch.backends.cudnn.benchmark = False
         use_gpu = True
-        kwargs = {'num_workers': config.num_workers,
-                  'pin_memory': config.pin_memory}
     else:
         use_gpu = False
     devices = get_devices(len(config.model_names), use_gpu)
 
-    # instantiate data loaders
-    data_dict = get_dataset(config.dataset, config.data_dir, 'test')
-    kwargs.update(data_dict)
+    # instantiate data load_teachers
+    data_dict = get_dataset(config.dataset, config.data_dir,
+                            'test', config.padding, config.padding_mode)
     config.num_classes = data_dict['num_classes']
+    print('Preparing the test loader...')
     test_loader = get_test_loader(
+        dataset=data_dict['dataset'],
+        num_classes=data_dict['num_classes'],
         batch_size=config.batch_size,
-        **kwargs
+        use_gpu=use_gpu,
+        progress_bar=config.progress_bar
     )
 
+    print('Preparing the validation loader...')
     if 'cifar' in config.dataset:
         valid_loader = test_loader
 
     else:
+        data_dict = get_dataset(
+            config.dataset, config.data_dir, 'val', config.padding, config.padding_mode)
         valid_loader = get_test_loader(
+            dataset=data_dict['dataset'],
+            num_classes=data_dict['num_classes'],
             batch_size=config.batch_size,
-            **kwargs
+            use_gpu=use_gpu,
+            progress_bar=config.progress_bar
         )
 
     if config.is_train:
-        data_dict = get_dataset(config.dataset, config.data_dir, 'train')
+        data_dict = get_dataset(
+            config.dataset, config.data_dir, 'train', config.padding, config.padding_mode)
         teachers = load_teachers(config, devices, data_dict['img_size'])
-        kwargs.update(data_dict)
-        train_loader = get_train_loader(batch_size=config.batch_size,
-                                        padding=config.padding,
-                                        padding_mode=config.padding_mode,
+        print('Preparing the training loader...')
+        train_loader = get_train_loader(dataset=data_dict['dataset'],
+                                        num_classes=data_dict['num_classes'],
+                                        batch_size=config.batch_size,
                                         random_seed=config.random_seed,
                                         shuffle=config.shuffle,
                                         model_num=len(config.model_names),
                                         teachers=teachers,
-                                        cuda=use_gpu,
-                                        **kwargs)
+                                        unlabel_split=config.unlabel_split,
+                                        use_gpu=use_gpu,
+                                        progress_bar=config.progress_bar)
     else:
         train_loader = None
 
